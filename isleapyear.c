0816328837fd4327a2831ecf85cfe4b59c6d9bb7
shokosobata0619 <sh8444@nyu.edu>
2025-11-11 06:06:45 -0500

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -1,18 +1,21 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {               #set arguement count & arguement vector
-    if (argc != 2) {                              #check number of arguements
-        printf("How to use: %s <year>\n", argv[0]);    #explain how to use the program
+int main(int argc, char *argv[]) {
+    // Check number of arguments
+    if (argc != 2) {
+        printf("How To Use: %s <year>\n", argv[0]);
         return 1;
     }
 
     int year = atoi(argv[1]);
 
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {  #check if leap year
-        printf("%d was a leap year\n", year);                      #confirm it was leap year
+    // Leap-year rule: divisible by 4 and not by 100, unless divisible by 400
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("%d was a leap year\n", year);
     } else {
-        printf("%d was not a leap year\n", year);                  #notify it was not leap year
+        printf("%d was not a leap year\n", year);
     }
 
     return 0;
